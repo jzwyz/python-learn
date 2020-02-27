47f97a4c29aee57b4754092e6c36de55e7f70f03
蒋周雯 <2146486290@qq.com>
2020-02-28 00:12:44 +0800

    a

--- test.py
+++ test.py
@@ -7,8 +7,6 @@ keyword 是python提供一个包,显示当前版本的所有关键字
 '''
 import keyword;
 
-
-
 # 单行注释 像这样
 print('Hello, World'); # 输出 字符串
 
@@ -17,3 +15,5 @@ print(keyword.kwlist);
 nums = [str(n) for n in keyword.kwlist]
 
 print(','.join(nums));
+
+# import this
